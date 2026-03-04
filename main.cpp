0f138df3298378a35badc4e7613b9f1faece74b4
Matthew Acosta <macos052@ucr.edu>
2026-03-04 00:55:35 -0800

    fixed forward selection

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include<fstream>
 #include<sstream>
 #include<vector>
-#include<set>
+#include<unordered_set>
 #include<cstdlib>
 
 using std::vector;
@@ -14,11 +14,11 @@ using std::to_string;
 using std::string;
 using std::ifstream;
 using std::istringstream;
-using std::set;
+using std::unordered_set;
 using std::rand;
 
-vector<vector<double>> loadData(const string& fileName){
-    vector<vector<double>> data;
+vector<vector<double> > loadData(const string& fileName){
+    vector<vector<double> > data;
     ifstream file(fileName);
     string line;
 
@@ -38,13 +38,13 @@ vector<vector<double>> loadData(const string& fileName){
     return data;
 }
 
-double leave_one_out_cross_validation(vector<vector<double>> data, set<int> current_set, int feature_to_add){
+double leave_one_out_cross_validation(vector<vector<double> > data, vector<int> current_set, int feature_to_add){
     return rand();
 }
 
-void forward_selection(vector<vector<double>> data);
+void forward_selection(vector<vector<double> > data);
 
-void backward_elimination(vector<vector<double>> data);
+void backward_elimination(vector<vector<double> > data);
 
 int main(){
     string fileName;
@@ -54,7 +54,7 @@ int main(){
 
     cin >> fileName;
 
-    vector<vector<double>> data = loadData(fileName);
+    vector<vector<double> > data = loadData(fileName);
 
     int answer = 0;
     do{
@@ -66,33 +66,37 @@ int main(){
         if(cin.fail()){
             throw std::runtime_error("Invalid input!");
         }
-    }while(answer != 1 || answer != 2);
+    }while(answer != 1 && answer != 2);
 
     if(answer == 1){
-
+        forward_selection(data);
     }
     else{
-
+        backward_elimination(data);
     }
     return 0;
 }
 
-void forward_selection(vector<vector<double>> data){
-    set<int> current_set_of_features;
+void forward_selection(vector<vector<double> > data){
+    unordered_set<int> current_set_of_features;
+    vector<int> best_overall_features; //tracks best pairing of features
 
-    for(int i = 1; i < data.size(); i++){
+    int total_features = data[0].size() - 1;
+    double best_overall_accuracy = 0;
+
+    for(int i = 1; i <= total_features; i++){
 
         cout << "On the " << to_string(i) << "th level of the search tree"<< endl;
-        
+
         int feature_to_add_to_this_level = 0;
         double best_so_far_acccuracy = 0;
 
-        for(int j = 1; j < data[i].size(); j++){
-            if(current_set_of_features.find(data[i][j]) == current_set_of_features.end()){
+        for(int j = 1; j <= total_features; j++){
+            if(current_set_of_features.find(j) == current_set_of_features.end()){
 
                 cout << "Considering adding the " << to_string(j) << " feature" << endl;
 
-                double accuracy = leave_one_out_cross_validation(data, current_set_of_features, j);
+                double accuracy = leave_one_out_cross_validation(data, best_overall_features, j);
 
                 if(accuracy > best_so_far_acccuracy){
                     best_so_far_acccuracy = accuracy;
@@ -100,9 +104,17 @@ void forward_selection(vector<vector<double>> data){
                 }
             }
         }
+
         current_set_of_features.insert(feature_to_add_to_this_level);
+
+        if(best_so_far_acccuracy > best_overall_accuracy){ //store best accuracy and features
+            best_overall_accuracy = best_so_far_acccuracy;
+            best_overall_features.push_back(feature_to_add_to_this_level);
+        }
 
         cout << "On level " << to_string(i) << " added feature "
         << to_string(feature_to_add_to_this_level) << " to current set"<< endl;
     }
 }
+
+void backward_elimination(vector<vector<double> > data){}
