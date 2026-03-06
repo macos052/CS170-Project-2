9a7dd79b089a441319debfcbfce02f84f3304d5a
Matthew Acosta <macos052@ucr.edu>
2026-03-05 22:29:07 -0800

    improve comments

--- main.cpp
+++ main.cpp
@@ -162,9 +162,9 @@ void forward_selection(vector<vector<double> > data){
                 double accuracy = leave_one_out_cross_validation(data, current_vec, j) * 100;
 
                 vector<int> output_vec = current_vec;
-                output_vec.push_back(j); //add feature to output
+                output_vec.push_back(j); // Add feature to output
 
-                //output feature and accuracy
+                // Output feature and accuracy
                 std::sort(output_vec.begin(), output_vec.end());
                 cout << "Using feature(s) {" << output_vec << "} accuracy is "
                 << std::fixed << std::setprecision(2) << accuracy << "%" << endl;
@@ -233,7 +233,7 @@ void backward_elimination(vector<vector<double> > data){
 
                 vector<int> output_vec = current_vec;
 
-                //output features and accuracy
+                // Output features and accuracy
                 std::sort(output_vec.begin(), output_vec.end());
                 cout << "Using feature(s) {" << output_vec << "} accuracy is "
                 << std::fixed << std::setprecision(2) << accuracy << "%" << endl;
