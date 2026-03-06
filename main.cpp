4ef0c3f8d75fdc1bdc9af873451cd5c59cbde94d
Matthew Acosta <macos052@ucr.edu>
2026-03-05 16:52:53 -0800

    paste in fowrard  into backward elkimination

--- main.cpp
+++ main.cpp
@@ -170,4 +170,45 @@ void forward_selection(vector<vector<double> > data){
     cout << "Finished search! The best feature subset is {" << best_overall_features << "}, which has an accuracy of " << std::fixed << std::setprecision(2) << best_overall_accuracy << "%" << endl;
 }
 
-void backward_elimination(vector<vector<double> > data){}
+void backward_elimination(vector<vector<double> > data){
+    unordered_set<int> current_set_of_features;
+    vector<int> best_overall_features; //tracks best pairing of features
+
+    int total_features = data[0].size() - 1;
+    double best_overall_accuracy = 0;
+
+    for(int i = 1; i <= total_features; i++){
+
+        cout << "On the " << to_string(i) << "th level of the search tree"<< endl;
+
+        int feature_to_add_to_this_level = 0;
+        double best_so_far_acccuracy = 0;
+
+        for(int j = 1; j <= total_features; j++){
+            if(current_set_of_features.find(j) == current_set_of_features.end()){
+                vector<int> current_vec(current_set_of_features.begin(), current_set_of_features.end());
+                double accuracy = leave_one_out_cross_validation(data, current_vec, j) * 100;
+
+                cout << "Considering adding the " << to_string(j) << " feature with accuracy " << std::fixed << std::setprecision(2) << accuracy << "%" << endl;
+
+                if(accuracy > best_so_far_acccuracy){
+                    best_so_far_acccuracy = accuracy;
+                    feature_to_add_to_this_level = j;
+                }
+            }
+        }
+        //always add best feature this level to current set
+        current_set_of_features.insert(feature_to_add_to_this_level);
+
+        if(best_so_far_acccuracy > best_overall_accuracy){ //store best accuracy and features
+
+            best_overall_accuracy = best_so_far_acccuracy;
+            best_overall_features.push_back(feature_to_add_to_this_level);
+
+            cout << "On level " << to_string(i) << " added feature "
+            << to_string(feature_to_add_to_this_level) << " to current set"<< endl;
+        }
+
+    }
+    cout << "Finished search! The best feature subset is {" << best_overall_features << "}, which has an accuracy of " << std::fixed << std::setprecision(2) << best_overall_accuracy << "%" << endl;
+}
