532dd96f9728ff4b2e261b05b324c1bd3741540a
Matthew Acosta <macos052@ucr.edu>
2026-03-05 19:42:09 -0800

    fixed backward elimination

--- main.cpp
+++ main.cpp
@@ -186,43 +186,50 @@ void forward_selection(vector<vector<double> > data){
 
 void backward_elimination(vector<vector<double> > data){
     unordered_set<int> current_set_of_features;
-    vector<int> best_overall_features; //tracks best subset features
+    vector<int> best_overall_features;
 
     int total_features = data[0].size() - 1;
     double best_overall_accuracy = 0;
+
+    // Start with all features
+    for(int i = 1; i <= total_features; i++){
+        current_set_of_features.insert(i);
+    }
 
     for(int i = 1; i <= total_features; i++){
 
         cout << "On the " << to_string(i) << "th level of the search tree"<< endl;
 
-        int feature_to_add_to_this_level = 0;
+        int feature_to_remove_this_level = 0;
         double best_so_far_acccuracy = 0;
 
         for(int j = 1; j <= total_features; j++){
-            if(current_set_of_features.find(j) == current_set_of_features.end()){
-                vector<int> current_vec(current_set_of_features.begin(), current_set_of_features.end());
-                double accuracy = leave_one_out_cross_validation(data, current_vec, j) * 100;
+            if(current_set_of_features.find(j) != current_set_of_features.end()){
+                // Temporarily remove j to test without it
+                unordered_set<int> temp_set = current_set_of_features;
+                temp_set.erase(j);
+                vector<int> current_vec(temp_set.begin(), temp_set.end());
+                double accuracy = leave_one_out_cross_validation(data, current_vec, 0) * 100;
 
-                cout << "Considering adding the " << to_string(j) << " feature with accuracy " << std::fixed << std::setprecision(2) << accuracy << "%" << endl;
+                cout << "Considering removing the " << to_string(j) << " feature with accuracy " << std::fixed << std::setprecision(2) << accuracy << "%" << endl;
 
                 if(accuracy > best_so_far_acccuracy){
                     best_so_far_acccuracy = accuracy;
-                    feature_to_add_to_this_level = j;
+                    feature_to_remove_this_level = j;
                 }
             }
         }
-        //always add best feature this level to current set
-        current_set_of_features.insert(feature_to_add_to_this_level);
+        // Always remove best feature this level
+        current_set_of_features.erase(feature_to_remove_this_level);
 
-        if(best_so_far_acccuracy > best_overall_accuracy){ //store best accuracy and features
-
+        if(best_so_far_acccuracy > best_overall_accuracy){
             best_overall_accuracy = best_so_far_acccuracy;
-            best_overall_features.push_back(feature_to_add_to_this_level);
+            best_overall_features = vector<int>(current_set_of_features.begin(), current_set_of_features.end());
 
-            cout << "On level " << to_string(i) << " added feature "
-            << to_string(feature_to_add_to_this_level) << " to current set"<< endl;
+            cout << "On level " << to_string(i) << " removed feature "
+            << to_string(feature_to_remove_this_level) << " from current set"<< endl;
         }
-
     }
+    std::sort(best_overall_features.begin(), best_overall_features.end());
     cout << "Finished search! The best feature subset is {" << best_overall_features << "}, which has an accuracy of " << std::fixed << std::setprecision(2) << best_overall_accuracy << "%" << endl;
 }
